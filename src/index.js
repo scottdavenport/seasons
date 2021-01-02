cbf8eb876432282a8ac2c0045044be38dad5ae77
scottdavenport <scott@pinpoint.com>
2021-01-02 09:29:19 -0500

    Avoiding Conditionals in Render

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ class App extends React.Component {
 		);
 	}
 
-	// react says we have to define render!!
-	render() {
+	renderContent() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error: {this.state.errorMessage} </div>;
 		}
@@ -25,6 +24,11 @@ class App extends React.Component {
 		}
 
 		return <Spinner message="Please accept location request" />;
+	}
+
+	// react says we have to define render!!
+	render() {
+		return <div className="border red">{this.renderContent()}</div>;
 	}
 }
 
